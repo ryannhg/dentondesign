b55e2dd38257a7b7b3e586863ef398f1bd93792f
Rene Hernandez <rhernandez@onenorth.com>
2018-10-02 23:16:08 -0500

    page heading structual and typography enhancements

--- components/PageHeading.vue
+++ components/PageHeading.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="page-heading wrapper wrapper--narrow">
-    <h1  class="page-heading__title">
-      <span class="sr-hidden">Nathan Denton Creative Director</span>
-      <span class="page-heading__svg-container">
+    <h1  class="page-heading__title"> 
+      <span class="sr-only">Nathan Denton Creative Director</span>
+      <span role="presentation" class="page-heading__svg-container">
         <img src="~/assets/images/letters-creativedirector.svg" class="page-heading__svg" />
       </span>
     </h1>
-    <div class="page-heading__copy fonts__h4">
-      <div class="fonts__h4">HATCHING new ideas for brands big and small</div>
+    <div class="page-heading__copy">
+      HATCHING new ideas for brands big and small
     </div>
   </div>  
 </template>
