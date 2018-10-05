0e0ecf1048729b00ca1078383aa4e6124e06cf1b
Rene Hernandez <rhernandez@onenorth.com>
2018-10-04 20:11:26 -0500

    cta links and minor spacing updates

---
+++ components/CtaLinks.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="cta-links">
+    <div class="cta-links__label fonts__h5">Explore</div>
+    <div class="cta-links__list button__row button__row--three-wide">
+      <nuxt-link to="/" class="button">work</nuxt-link>
+      <nuxt-link to="/" class="button">work</nuxt-link>
+      <nuxt-link to="/" class="button">work</nuxt-link>
+    </div>
+  </nav>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <parallax-background></parallax-background>
     <div class="layout__content">
       <page-heading></page-heading>
+      <cta-links></cta-links>
     </div>
   </div>
 </template>
@@ -11,16 +12,17 @@
 <script>
 import ParallaxBackground from '~/components/ParallaxBackground'
 import PageHeading from '~/components/PageHeading'
+import CtaLinks from '~/components/CtaLinks'
 import page from '~/static/content/home.json'
 
 export default {
   components: {
     ParallaxBackground,
-    PageHeading
+    PageHeading,
+    CtaLinks
   },
   data: () => ({
     page
   })
 }
 </script>
-
