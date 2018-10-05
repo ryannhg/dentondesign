8bf29f9fbfe1b1683c7c38bec9565a2021cceeb6
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2018-10-04 20:47:45 -0500

    chicken love

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="layout__main">
-    <parallax-background :image="page.backgroundImage.src"></parallax-background>
-    <div class="layout__content">
+    <parallax-background :image="page.backgroundImage.src" :percent="percent"></parallax-background>
+    <div class="layout__content" v-percentage="percent">
+      <div style="height: 86vh"></div>
       <page-heading :title="page.caption" :image="page.titleImage"></page-heading>
     </div>
   </div>
@@ -9,6 +10,7 @@
 
 
 <script>
+import Percentage from '~/plugins/Percentage'
 import ParallaxBackground from '~/components/ParallaxBackground'
 import PageHeading from '~/components/PageHeading'
 import page from '~/static/content/home.json'
@@ -19,7 +21,8 @@ export default {
     PageHeading
   },
   data: () => ({
-    page
+    page,
+    percent: 0
   })
 }
 </script>
