d654d44e74d1f0df2476dbb7b3fb90e4e9805f9d
Rene Hernandez <rhernandez@onenorth.com>
2018-10-10 19:23:44 -0500

    burger and menum and adding titles for work

---
+++ components/Burger.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="burger">
+    <span class="burger__buns"></span>
+    <span class="burger__fillings"></span>
+    <span class="burger__buns burger__buns--bottom"></span>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  props: { isExpanded: Boolean }
+}
+</script>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,22 +1,24 @@
 <template>
   <nav class="navigation">
     <div class="navigation__menu">
+      <burger></burger>
+    </div>
+    <div class="navigation__egg">
       <div class="navigation__svg-container">
-      <!-- <svg class="menu__egg-svg" viewBox="0 0 141 126" xmlns="http://www.w3.org/2000/svg"><path d="M61.702 126C23.14 126 0 101.24 0 63 0 24.761 23.14 0 61.702 0 100.262 0 141 24.761 141 63c0 38.24-40.737 63-79.298 63z" fill="#C21E4E" fill-rule="evenodd"/></svg> -->
-        <img class="navigation__egg" src="~/assets/images/eggmenu.svg" />
+        <!-- <svg class="menu__egg-svg" viewBox="0 0 141 126" xmlns="http://www.w3.org/2000/svg"><path d="M61.702 126C23.14 126 0 101.24 0 63 0 24.761 23.14 0 61.702 0 100.262 0 141 24.761 141 63c0 38.24-40.737 63-79.298 63z" fill="#C21E4E" fill-rule="evenodd"/></svg> -->
+        <img class="navigation__egg-svg" src="~/assets/images/eggmenu.svg" />
       </div>
-      <span class="navigation__burger">
-        <span class="navigation__burger-buns"></span>
-        <span class="navigation__burger-fillings"></span>
-        <span class="navigation__burger-buns navigation__burger-buns--bottom"></span>
-      </span>
     </div>
   </nav>
 </template>
 
 <script>
+import Burger from '~/components/Burger'
 
 export default {
+  components: {
+    Burger
+  },
   props: ['content']
 }
 </script>
