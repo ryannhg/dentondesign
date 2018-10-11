2c66a85a9763a62c3d06783ecbac6245e7a4891d
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2018-10-10 19:33:29 -0500

    work things

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -3,25 +3,27 @@
     <parallax-background :image="page.backgroundImage.src"></parallax-background>
     <div class="layout__content">
       <page-heading :title="page.caption" :image="page.titleImage"></page-heading>
-      <cta-links v-bind="page.explore"></cta-links>
+      <div class="work-links">
+        <nuxt-link class="work-links__link"
+          v-for="(link, i) in page.links" :key="i"
+          :to="link.url" v-text="link.label">
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
 
-
 <script>
 import ParallaxBackground from '~/components/ParallaxBackground'
 import PageHeading from '~/components/PageHeading'
-import CtaLinks from '~/components/CtaLinks'
-import page from '~/static/content/home.json'
+import page from '~/static/content/work.json'
 import Page from '~/plugins/Page'
 
 export default {
   mixins: [ Page ],
   components: {
     ParallaxBackground,
-    PageHeading,
-    CtaLinks
+    PageHeading
   },
   data: () => ({ page })
 }
