8d9e917273539978a34431dd8a7a4255477c3222
rbhernan@gmail.com <rhernandez@onenorth.com>
2018-10-28 14:06:02 -0500

    content for work detail

--- pages/work/_slug.vue
+++ pages/work/_slug.vue
@@ -2,6 +2,13 @@
 <main class="layout__main">
   <div class="layout__content layout__content--above-fold">
     <page-heading :caption="page.description" :image="page.titleImage"></page-heading>
+
+    <line-snippet v-if="hasSnippetFor('Obstacle')" 
+      :title="'Obstacle'" :copy="page.obstacle" :image-type="'egg'"></line-snippet>
+    <line-snippet v-if="hasSnippetFor('Objective')" 
+      :title="'Objective'" :copy="page.objective" :image-type="'shell'"></line-snippet>
+    <line-snippet v-if="hasSnippetFor('Outcome')" 
+      :title="'Outcome'" :copy="page.outcome" :image-type="'sandwich'"></line-snippet>
   </div>
 </main>
 </template>
@@ -9,13 +16,20 @@
 
 <script>
 import pageHeading from '~/components/PageHeading'
+import LineSnippet from '~/components/LineSnippet'
 import page from '~/static/content/work/burns-mcdonnell.json'
 
 
 export default {
   components: {
-    pageHeading
+    pageHeading,
+    LineSnippet
   },
-  data: () =>({page})
+  data: () =>({page}),
+  methods: {
+    hasSnippetFor (section) {
+      return this.$data.page[section.toLowerCase()] 
+    }
+  }
 }
 </script>
