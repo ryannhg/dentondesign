1eeb23ddd4f46fdbb8ea1ef1bf42495662a36817
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2018-11-04 18:06:44 -0600

    add in google maps link for address, use tel instead of mailto for phone

--- components/Digits.vue
+++ components/Digits.vue
@@ -4,10 +4,10 @@
       <span class="digits__prefix">E</span> <a class="digits__contact" :href="'mailto:' + contactInfo.email">{{contactInfo.email}}</a>
     </p>
     <p class="digits__entry fonts__small">
-      <span class="digits__prefix">P</span> <a class="digits__contact digits__contact--dark-link" :href="'mailto:' + contactInfo.phone">{{contactInfo.phone}}</a>
+      <span class="digits__prefix">P</span> <a class="digits__contact digits__contact--dark-link" :href="'tel:' + contactInfo.phone">{{contactInfo.phone}}</a>
     </p>
     <address class="digits__entry fonts__small">
-      <span class="digits__prefix">E</span> <div class="digits__contact"  v-html="marked(contactInfo.address)"></div>
+      <span class="digits__prefix">A</span> <a class="digits__contact" target="_blank" :href="addressLink" v-html="marked(contactInfo.address)"></a>
     </address>
   </div>
 </template>
@@ -18,6 +18,13 @@ import marked from 'marked'
 
 export default {
   props: { contactInfo: Object },
+  computed: {
+    addressLink () {
+      return this.contactInfo.address
+        ? `https://maps.google.com?q=${this.contactInfo.address.split('\n').join(' ')}`
+        : ''
+    }
+  },
   methods: {
     marked
   }
